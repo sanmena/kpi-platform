39b627a67ffe4266a95c182b7912f2bd55bdf2bf
saberuster <saberuster@gmail.com>
2017-08-05 11:10:18 +0800

    #框架基础代码提交

---
+++ src/main/java/platform/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package platform.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String home() {
+        return "hello world!";
+    }
+}
